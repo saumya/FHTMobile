f40026ad65d9fc54d3788bebc969acccb320b343
saumya <2saumyaray@gmail.com>
2018-11-01 16:15:25 +0530

    home page

--- App.js
+++ App.js
@@ -7,7 +7,12 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {  
+  Platform, 
+  StyleSheet, 
+  Text,
+  WebView, ScrollView, Image, 
+  View,  } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,9 +26,21 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <ScrollView style={{backgroundColor:'#ffffff', width:'95%'}}>
+      
+        <Text style={styles.welcome} numberOfLines={5}> Find Health </Text>
+        <View style={{alignItems: 'center', justifyContent:'center'}}>
+          <Image source={require('./fht.android.jpg')} style={{width: 400, height: 320}} />
+        </View>
+        <Text style={styles.welcomeMessage}> 
+          Today. Being healthy means taking medicines from costly Hospitals, taking regular check-ups at most costly checkup centers. This is going on as if its a competition of who visits the most costly Hospital. 
+        </Text>
+        <View style={{height:2, marginTop:10, marginBottom:2, backgroundColor:'#cccccc'}}></View>
+        <Text style={styles.welcomeMessage}> 
+          Being healthy is simple and we are here to help. 
+        </Text>
+      
+      </ScrollView>
       </View>
     );
   }
@@ -37,13 +54,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 30,
     textAlign: 'center',
     margin: 10,
+  },
+  welcomeMessage:{
+    fontSize: 20,
+    textAlign: 'left',
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  WebViewContainer: {
+    marginTop: (Platform.OS == 'ios') ? 20 : 0, 
+    width:300,
+    height:100, 
+    justifyContent:'center', 
+    alignItems:'center', 
+    backgroundColor:'black',
   },
 });
---
+++ README.md
@@ -0,0 +1,15 @@
+FindHealth.Today
+==========================
+
+### References:
+ Home Page [Image][img1]
+
+
+
+
+
+
+
+
+[img1]: https://pixabay.com/en/people-woman-yoga-meditation-2562357/
+
